408abe991f931a5489fc7a73f63ce5117c4d2292
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-23 10:56:39 +0900

    feat: 테스용 페이지 row type 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,45 @@ import {
 function App() {
   return (
     <>
-      <TimeTable
-        totalStartTime={startTime}
-        totalEndTime={endTime}
-        slotRange={slotTime}
-        taskList={taskListWithouttaskColor}
-        timeTableSize="2000px"
-        timetableDirection="COLUMN"
-        displayCurrentTime
-        timeTableStyle={{ backgroundColor: 'white' }}
-        timeSlotStyle={{ color: 'black' }}
-        taskSlotStyle={{ color: 'black' }}
-        ellipsisText="..."
-      />
+      <div style={{ height: '500px' }}>
+        <TimeTable
+          totalStartTime={startTime}
+          totalEndTime={endTime}
+          slotRange={slotTime}
+          taskList={taskListWithouttaskColor}
+          timeTableSize="2000px"
+          timetableDirection="COLUMN"
+          displayCurrentTime
+          timeTableStyle={{ backgroundColor: 'white' }}
+          timeSlotStyle={{ color: 'black', fontSize: '20px' }}
+          taskSlotStyle={{
+            color: 'black',
+            textShadow: '1px 1px 1px gray',
+            fontSize: '32px',
+          }}
+          ellipsisText=""
+        />
+      </div>
+
+      <div style={{ height: '500px' }}>
+        <TimeTable
+          totalStartTime={startTime}
+          totalEndTime={endTime}
+          slotRange={slotTime}
+          taskList={taskListWithouttaskColor}
+          timeTableSize="2000px"
+          timetableDirection="ROW"
+          displayCurrentTime
+          timeTableStyle={{ backgroundColor: 'white' }}
+          timeSlotStyle={{ color: 'black' }}
+          taskSlotStyle={{
+            color: 'black',
+            textShadow: '1px 1px 1px gray',
+            fontSize: '32px',
+          }}
+          ellipsisText="..."
+        />
+      </div>
     </>
   );
 }
